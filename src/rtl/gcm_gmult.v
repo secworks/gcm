5dc185e31deedd97e9b270ddc1ce4d5e61266799
Joachim Strömbergson <joachim@secworks.se>
2018-01-29 12:03:31 +0100

    Updated the gmult to the naive test code. Not at all sure it is correct.

--- src/rtl/gcm_gmult.v
+++ src/rtl/gcm_gmult.v
@@ -39,87 +39,74 @@
 module gcm_gmult(
                  input wire            clk,
                  input wire            reset_n,
-
-                 input wire            next,
-                 output wire           ready,
-
-                 input wire [127 : 0]  a,
-                 input wire [127 : 0]  b,
+                 input wire [127 : 0]  opa,
                  output wire [127 : 0] res
                 );
-
-  //----------------------------------------------------------------
-  // Defines.
-  //----------------------------------------------------------------
-  localparam CTRL_IDLE = 2'h0;
-  localparam CTRL_INIT = 2'h1;
-  localparam CTRL_CTR  = 2'h2;
-  localparam CTRL_DONE = 2'h3;
 
 
   //----------------------------------------------------------------
   // Gaolis multiplication functions.
   //----------------------------------------------------------------
-  function [7 : 0] gm2(input [7 : 0] op);
+  function [127 : 0] gm2(input [127 : 0] op);
     begin
-      gm2 = {op[6 : 0], 1'b0} ^ (8'h1b & {8{op[7]}});
+      gm2 = {op[126 : 0], 1'b0} ^ (128'h1b & {128{op[127]}});
     end
   endfunction // gm2
 
-  function [7 : 0] gm3(input [7 : 0] op);
+  function [127 : 0] gm3(input [127 : 0] op);
     begin
       gm3 = gm2(op) ^ op;
     end
   endfunction // gm3
 
-  function [7 : 0] gm4(input [7 : 0] op);
+  function [127 : 0] gm4(input [127 : 0] op);
     begin
       gm4 = gm2(gm2(op));
     end
   endfunction // gm4
 
-  function [7 : 0] gm7(input [7 : 0] op);
+  function [127 : 0] gm7(input [127 : 0] op);
     begin
-      gm7 = gm3(op) ^ gm3(op) ^ gm2(op);
+      gm7 = gm4(op) ^ gm3(op);
     end
-  endfunction // gm09
+  endfunction // gm7
 
-  function [7 : 0] gm8(input [7 : 0] op);
+  function [127 : 0] gm8(input [127 : 0] op);
     begin
       gm8 = gm2(gm4(op));
     end
   endfunction // gm8
 
-  function [7 : 0] gm128(input [7 : 0] op);
+  function [127 : 0] gm16(input [127 : 0] op);
     begin
-      gm128 = gm2(gm8(gm8(op)));
+      gm16 = gm4(gm4(op));
     end
-  endfunction // gm8
+  endfunction // gm16
+
+  function [127 : 0] gm128(input [127 : 0] op);
+    begin
+      gm128 = gm8(gm16(op));
+    end
+  endfunction // gm128
+
 
   //----------------------------------------------------------------
-  // Registers.
+  // Registers including update variables and write enable.
   //----------------------------------------------------------------
-  reg           ready_reg;
-  reg           ready_new;
-
-  reg [127 : 0] res_reg;
+  reg [127 : 0] op_reg;
   reg [127 : 0] res_new;
-  reg           res_we;
+  reg [127 : 0] res_reg;
 
-  reg [1 : 0]   gmult_ctrl_reg;
-  reg [1 : 0]   gmult_ctrl_new;
-  reg           gmult_ctrl_we;
+  reg [127 : 0] pipe1_reg;
+  reg [127 : 0] pipe1_new;
+  reg [127 : 0] pipe2_reg;
+  reg [127 : 0] pipe2_new;
 
-
-  //----------------------------------------------------------------
-  // Wires.
-  //----------------------------------------------------------------
 
   //----------------------------------------------------------------
-  // Concurrent assignments.
+  // Concurrent connectivity for ports etc.
   //----------------------------------------------------------------
-  assign ready = ready_reg;
-  assign res   = res_reg;
+  assign res = res_reg;
 
 
   //----------------------------------------------------------------
@@ -127,78 +114,36 @@ module gcm_gmult(
   //
   // Update functionality for all registers in the core.
   // All registers are positive edge triggered with synchronous
-  // active low reset.
+  // active low reset. All registers have write enable.
   //----------------------------------------------------------------
-  always @ (posedge clk)
-    begin : reg_update
+  always @ (posedge clk or negedge reset_n)
+    begin: reg_update
       if (!reset_n)
         begin
-          ready_reg <= 0;
+          op_reg    <= 128'h0;
           res_reg   <= 128'h0;
+          pipe1_reg <= 128'h0;
+          pipe2_reg <= 128'h0;
         end
       else
         begin
-          ready_reg <= ready_new;
-
-          if (res_we)
-            res_reg <= res_new;
-
-          if (gmult_ctrl_we)
-            gmult_ctrl_reg <= gmult_ctrl_new;
+          op_reg    <= op;
+          res_reg   <= res_new;
+          pipe1_reg <= pipe1_new;
+          pipe2_reg <= pipe2_new;
         end
     end // reg_update
 
 
   //----------------------------------------------------------------
-  // gmult_dp
+  // gmult_logic
   //----------------------------------------------------------------
   always @*
-    begin : gmult_dp
-      res_we = 0;
-      res_new = 128'h0;
-
-    end // gmult_dp
-
-
-  //----------------------------------------------------------------
-  // gmult_ctrl
-  //----------------------------------------------------------------
-  always @*
-    begin : gmult_ctrl
-      ready_new     = 0;
-      gmult_ctrl_new = CTRL_IDLE;
-      gmult_ctrl_we  = 0;
-
-      case (gmult_ctrl_reg)
-        CTRL_IDLE:
-          begin
-            ready_new = 1;
-            if (next)
-              begin
-                gmult_ctrl_new = CTRL_INIT;
-                gmult_ctrl_we  = 1;
-              end
-          end
-
-        CTRL_INIT:
-          begin
-            gmult_ctrl_new = CTRL_IDLE;
-            gmult_ctrl_we  = 1;
-          end
-
-        CTRL_CTR:
-          begin
-          end
-
-        CTRL_DONE:
-          begin
-          end
-
-        default:
-          begin
-          end
-      endcase // case (gmult_ctrl_reg)
-    end // gmult_ctrl
+    begin : gmult_logic
+      pipe1_new = gm128(op_reg);
+      pipe2_new = gm7(op_reg) + gm2(op_reg) + op_reg + 1'b1;
+      res_new = pipe1_reg + pipe2_reg;
+    end // gmult_logic
 
 endmodule // gcm_gmult
 
