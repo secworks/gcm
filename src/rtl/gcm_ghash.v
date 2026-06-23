ce3c7bcf5963a761148cf8ad37474a63e5baaede
Joachim Strömbergson <joachim@assured.se>
2026-06-23 12:44:59 +0200

    Add first working implementation of the ghash module
        Add working version of the ghash module. Takes 16 cycles to
        produce a hash (y in the SP 800-38D spec).
    
    Signed-off-by: Joachim Strömbergson <joachim@assured.se>

--- src/rtl/gcm_ghash.v
+++ src/rtl/gcm_ghash.v
@@ -6,7 +6,7 @@
 //
 //
 // Author: Joachim Strombergson
-// Copyright (c) 2018, Secworks Sweden AB
+// Copyright (c) 2018, 2026 Assured AB
 // All rights reserved.
 //
 // Redistribution and use in source and binary forms, with or
@@ -36,122 +36,260 @@
 //
 //======================================================================
 
+`default_nettype none
+
 module gcm_ghash(
-                 input wire            clk,
-                 input wire            reset_n,
+                 input  wire         clk,
+                 input  wire         reset_n,
 
-                 input wire            init,
-                 input wire            next,
+                 input  wire         init,
+                 input  wire         next,
 
-                 input wire [127 : 0]  h0,
-                 input wire [127 : 0]  x,
-                 output wire [127 : 0] y,
-                 output wire           ready
+                 input  wire [127:0] h,
+                 input  wire [127:0] block,
+
+                 output wire         ready,
+                 output wire [127:0] y
                 );
 
+  //----------------------------------------------------------------
+  // Internal constant and parameter definitions.
+  //----------------------------------------------------------------
+  localparam CTRL_IDLE  = 1'h0;
+  localparam CTRL_ROUND = 1'h1;
+
+  localparam LOGIC_IDLE = 2'h0;
+  localparam LOGIC_INIT = 2'h1;
+  localparam LOGIC_NEXT = 2'h2;
+  localparam LOGIC_GMUL = 2'h3;
 
   //----------------------------------------------------------------
-  // Gaolis multiplication functions.
+  // Registers including update vectors and contol.
   //----------------------------------------------------------------
-  function [127 : 0] gm2(input [127 : 0] op);
-    begin
-      gm2 = {op[126 : 0], 1'b0} ^ (128'h1b & {128{op[127]}});
-    end
-  endfunction // gm2
+  reg [127:0] h_reg;
+  reg [127:0] h_new;
+  reg         h_we;
 
-  function [127 : 0] gm3(input [127 : 0] op);
-    begin
-      gm3 = gm2(op) ^ op;
-    end
-  endfunction // gm3
+  reg [127:0] v_reg;
+  reg [127:0] v_new;
+  reg         v_we;
 
-  function [127 : 0] gm4(input [127 : 0] op);
-    begin
-      gm4 = gm2(gm2(op));
-    end
-  endfunction // gm4
+  reg [127:0] x_reg;
+  reg [127:0] x_new;
+  reg         x_we;
 
-  function [127 : 0] gm7(input [127 : 0] op);
-    begin
-      gm7 = gm4(op) ^ gm3(op);
-    end
-  endfunction // gm7
+  reg [127:0] y_reg;
+  reg [127:0] y_new;
+  reg         y_we;
 
-  function [127 : 0] gm8(input [127 : 0] op);
-    begin
-      gm8 = gm2(gm4(op));
-    end
-  endfunction // gm8
+  reg [3:0]  round_ctr_reg;
+  reg [3:0]  round_ctr_new;
+  reg        round_ctr_rst;
+  reg        round_ctr_inc;
+  reg        round_ctr_we;
 
-  function [127 : 0] gm16(input [127 : 0] op);
-    begin
-      gm16 = gm4(gm4(op));
-    end
-  endfunction // gm16
+  reg        ready_reg;
+  reg        ready_new;
+  reg        ready_we;
 
-  function [127 : 0] gm128(input [127 : 0] op);
-    begin
-      gm128 = gm8(gm16(op));
-    end
-  endfunction // gm128
-
+  reg        gcm_ghash_ctrl_reg;
+  reg        gcm_ghash_ctrl_new;
+  reg        gcm_ghash_ctrl_we;
 
   //----------------------------------------------------------------
-  // Registers including update variables and write enable.
+  // wires
   //----------------------------------------------------------------
-  reg [127 : 0] op_reg;
-  reg [127 : 0] res_new;
-  reg [127 : 0] res_reg;
-
-  reg [127 : 0] pipe1_reg;
-  reg [127 : 0] pipe1_new;
-  reg [127 : 0] pipe2_reg;
-  reg [127 : 0] pipe2_new;
-
+  reg [1 : 0] logic_ctrl;
 
   //----------------------------------------------------------------
-  // Concurrent connectivity for ports etc.
+  // Funnctions.
   //----------------------------------------------------------------
-  assign y = res_reg;
+  function automatic [127 : 0] gf_shift(input [127:0] v);
+    begin
+      if (v[0])
+        gf_shift = (v >> 1) ^ 128'he1000000000000000000000000000000;
+      else
+        gf_shift = (v >> 1);
+    end
+  endfunction // gf_shift
 
+  //----------------------------------------------------------------
+  // Concurrent connectivity for ports.
+  //----------------------------------------------------------------
+  assign ready = ready_reg;
+  assign y     = y_reg;
 
   //----------------------------------------------------------------
   // reg_update
-  //
-  // Update functionality for all registers in the core.
-  // All registers are positive edge triggered with synchronous
-  // active low reset. All registers have write enable.
   //----------------------------------------------------------------
-  always @ (posedge clk or negedge reset_n)
-    begin: reg_update
-      if (!reset_n)
-        begin
-          op_reg    <= 128'h0;
-          res_reg   <= 128'h0;
-          pipe1_reg <= 128'h0;
-          pipe2_reg <= 128'h0;
-        end
-      else
-        begin
-          op_reg    <= op;
-          res_reg   <= res_new;
-          pipe1_reg <= pipe1_new;
-          pipe2_reg <= pipe2_new;
-        end
-    end // reg_update
+  always @(posedge clk) begin
+    if (!reset_n) begin
+      h_reg              <= 128'h0;
+      y_reg              <= 128'h0;
+      v_reg              <= 128'h0;
+      x_reg              <= 128'h0;
+      round_ctr_reg      <= 4'h0;
+      gcm_ghash_ctrl_reg <= CTRL_IDLE;
+      ready_reg          <= 1'h1;
+    end
 
+    else begin
+      if (h_we) begin
+        h_reg <= h_new;
+      end
+
+      if (v_we) begin
+        v_reg <= v_new;
+      end
+
+      if (x_we) begin
+        x_reg <= x_new;
+      end
+
+      if (y_we) begin
+        y_reg <= y_new;
+      end
+
+      if (round_ctr_we) begin
+        round_ctr_reg <= round_ctr_new;
+      end
+
+      if (ready_we) begin
+        ready_reg <= ready_new;
+      end
+
+      if (gcm_ghash_ctrl_we) begin
+        gcm_ghash_ctrl_reg <= gcm_ghash_ctrl_new;
+      end
+    end
+  end
 
   //----------------------------------------------------------------
-  // gmult_logic
+  // round_ctr
   //----------------------------------------------------------------
   always @*
-    begin : gmult_logic
-      pipe1_new = gm128(op_reg);
-      pipe2_new = gm7(op_reg) + gm2(op_reg) + op_reg + 1'b1;
-      res_new = pipe1_reg + pipe2_reg;
-    end // gmult_logic
+    begin: round_ctr
+      round_ctr_new = 4'h0;
+      round_ctr_we  = 1'h0;
 
-endmodule // gcm_gmult
+      if (round_ctr_rst) begin
+        round_ctr_new = 4'h0;
+        round_ctr_we  = 1'h1;
+        end
+      else if (round_ctr_inc) begin
+        round_ctr_new = round_ctr_reg + 1'h1;
+        round_ctr_we  = 1'h1;
+      end
+    end
+
+  //----------------------------------------------------------------
+  // ghash_logic
+  //----------------------------------------------------------------
+  always @*
+    begin: round_logic
+      integer     i;
+
+      h_new = 128'h0;
+      v_new = 128'h0;
+      x_new = 128'h0;
+      y_new = 128'h0;
+      h_we  = 1'h0;
+      v_we  = 1'h0;
+      x_we  = 1'h0;
+      y_we  = 1'h0;
+
+      case (logic_ctrl)
+        LOGIC_IDLE: begin
+          // We do nothing
+        end
+
+        LOGIC_INIT: begin
+          h_new = h;
+          v_new = 128'h0;
+          x_new = 128'h0;
+          y_new = 128'h0;
+          h_we  = 1'h1;
+          v_we  = 1'h1;
+          x_we  = 1'h1;
+          y_we  = 1'h1;
+        end
+
+        LOGIC_NEXT: begin
+          v_new = h_reg;
+          v_we  = 1'h1;
+          x_new = y_reg ^ block;
+          x_we  = 1'h1;
+          y_new = 128'h0;
+          y_we  = 1'h1;
+        end
+
+        LOGIC_GMUL: begin
+          v_new = v_reg;
+          x_new = x_reg;
+          y_new = y_reg;
+
+          for (i = 0; i < 8; i = i + 1) begin
+            if (x_new[127]) begin
+              y_new = y_new ^ v_new;
+            end
+            v_new = gf_shift(v_new);
+            x_new = {x_new[126:0], 1'b0};
+          end
+          v_we  = 1'h1;
+          x_we  = 1'h1;
+          y_we  = 1'h1;
+        end
+
+        default: begin end
+      endcase // case (logic_ctrl)
+    end
+
+  //----------------------------------------------------------------
+  // gcm_ghash_crtl
+  //----------------------------------------------------------------
+  always @*
+    begin: gcm_ghash_ctrl
+      ready_new          = 1'h0;
+      ready_we           = 1'h0;
+      round_ctr_rst      = 1'h0;
+      round_ctr_inc      = 1'h0;
+      logic_ctrl         = LOGIC_IDLE;
+      gcm_ghash_ctrl_new = CTRL_IDLE;
+      gcm_ghash_ctrl_we  = 1'h0;
+
+      case(gcm_ghash_ctrl_reg)
+        CTRL_IDLE: begin
+          if (init) begin
+            logic_ctrl = LOGIC_INIT;
+          end
+
+          if (next) begin
+            ready_new     = 1'h0;
+            ready_we      = 1'h1;
+            round_ctr_rst = 1'h0;
+            logic_ctrl    = LOGIC_NEXT;
+            gcm_ghash_ctrl_new = CTRL_ROUND;
+            gcm_ghash_ctrl_we  = 1'h1;
+          end
+        end
+
+        CTRL_ROUND: begin
+          logic_ctrl    = LOGIC_GMUL;
+          round_ctr_inc = 1'h1;
+
+          if (round_ctr_reg == 4'hf) begin
+            ready_new          = 1'h1;
+            ready_we           = 1'h1;
+            gcm_ghash_ctrl_new = CTRL_IDLE;
+            gcm_ghash_ctrl_we  = 1'h1;
+          end
+        end
+
+        default: begin end
+      endcase // case (gcm_ghash_ctrl_reg)
+    end
+
+endmodule // gcm_ghash
 
 //======================================================================
 // EOF gcm_ghash.v
